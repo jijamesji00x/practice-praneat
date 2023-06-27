e140286f11ffc89d4d288238b634937a89e9008a
jijamesji00x <91174069+jijamesji00x@users.noreply.github.com>
2023-06-27 15:42:59 +0700

    Update init.sql

--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -15,4 +15,7 @@ CREATE TABLE IF NOT EXISTS country_and_capitals (
     INSERT INTO country_and_capitals(country,capital) VALUES(
         'France','Paris'
     );
+    INSERT INTO country_and_capitals(country,capital) VALUES(
+        'Singapore','Singapore'
+    );
     
