1b14792919f6b57be01ac9786a3d8a2132d7a551
Phirawat Jeerapiwat <phirawat@praneat.com>
2024-11-07 19:04:16 +0700

    refactor: 💡 code change

--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -1,24 +1,47 @@
-CREATE TABLE IF NOT EXISTS country_and_capitals (
-  country varchar(255) ,
-  capital varchar(255) 
-);
+CREATE TABLE
+    IF NOT EXISTS country_and_capitals (country varchar(255), capital varchar(255));
 
-    INSERT INTO country_and_capitals(country,capital) VALUES(
-    'Thailand','Bangkok'
-    );
-    INSERT INTO country_and_capitals(country,capital) VALUES(
-    'China','Beijing'
-    );
-    INSERT INTO country_and_capitals(country,capital) VALUES(
-        'Japan','Tokyo'
-    );
-    INSERT INTO country_and_capitals(country,capital) VALUES(
-        'France','Paris'
-    );
-    INSERT INTO country_and_capitals(country,capital) VALUES(
-        'Singapore','Singapore'
-    );
-    INSERT INTO country_and_capitals(country,capital) VALUES(
-        'New-Zealand','Wellington'
-    );
-    
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('Thailand', 'Bangkok');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('China', 'Beijing');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('Japan', 'Tokyo');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('France', 'Paris');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('Singapore', 'Singapore');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('New-Zealand', 'Wellington');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('Thailand', 'Ranong');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('Thailand', 'Rayong');
+
+INSERT INTO
+    country_and_capitals (country, capital)
+VALUES
+    ('Thailand', 'Yala');
